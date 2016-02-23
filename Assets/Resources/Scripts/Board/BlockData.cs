a56399e196909743f21908a2997f9bdc54034a9a
ardiwii <ardi.wii@gmail.com>
2016-02-24 01:01:26 +0700

    board first commit

---
+++ Assets/Resources/Scripts/Board/BlockData.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockData : MonoBehaviour {
+
+	//height of the block, represented by the number of children tiles, stacked in the block, >=0, 0 means water tile
+	public int altitude;
+
+	//the object on the block, the object contained in a block is placed as the last child of that block.
+	public GameObject contains;
+
+	//block x and y coordinate in the board
+	public int x;
+	public int y;
+
+	//tile prefabs
+	[SerializeField] private GameObject waterTile;
+	[SerializeField] private GameObject grassTile;
+	[SerializeField] private GameObject borderTile;
+
+	//the height difference between stacked tiles
+	private float deltaY = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	//Generate tiles with default altitude, for non default altitude, use only if altitude has been set manually before calling this method
+	public void GenerateTiles(){
+		if (altitude == 0) {
+			GameObject newTile = (GameObject) Instantiate (waterTile,transform.position,new Quaternion());
+			newTile.transform.SetParent (transform);
+		}
+		else{
+			for (int i = 1; i <= altitude; i++) {
+				GameObject newTile = (GameObject) Instantiate (grassTile,new Vector3(transform.position.x,transform.position.y+(i-1)*deltaY,transform.position.z),new Quaternion());
+				newTile.transform.SetParent (transform);
+			}
+		}
+	}
+
+	//Generate tiles according to altitude set in alt parameter
+	public void GenerateTiles(int alt){
+		altitude = alt;
+		if (altitude == 0) {
+			GameObject newTile = (GameObject) Instantiate (waterTile,transform.position,new Quaternion());
+			newTile.transform.SetParent (transform);
+		}
+		else{
+			for (int i = 1; i <= altitude; i++) {
+				GameObject newTile = (GameObject) Instantiate (grassTile,new Vector3(transform.position.x,transform.position.y+(i-1)*deltaY,transform.position.z),new Quaternion());
+				newTile.transform.SetParent (transform);
+			}
+		}
+	}
+
+	//Generate border tiles
+	public void GenerateBorderTiles(){
+		if (altitude == 0) {
+			GameObject newTile = (GameObject) Instantiate (borderTile,transform.position,new Quaternion());
+			newTile.transform.SetParent (transform);
+		}
+		else{
+			for (int i = 1; i <= altitude; i++) {
+				GameObject newTile = (GameObject) Instantiate (borderTile,new Vector3(transform.position.x,transform.position.y+(i-1)*deltaY,transform.position.z),new Quaternion());
+				newTile.transform.SetParent (transform);
+			}
+		}
+	}
+
+	//PUBLIC METHOD FOR UNIT MOVEMENT
+	public void InsertObject(){
+	
+	}
+}
---
+++ Assets/Resources/Scripts/Board/TileGenerator.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//NOTE: Blocks of a board with 8x8 size will be from x=0 to x=11 (+2 for border) relative to the position of the board object in unity world space,
+// same rule applied in y
+
+//all blocks in a board is children of the board in hierarchy
+
+public class TileGenerator : MonoBehaviour {
+
+	public int boardWidth;
+	public int boardLength;
+
+	//board code for accessing the block inside this board, could be player's number
+	public int boardCode;
+
+	//Block Prefab
+	[SerializeField] GameObject BlockPrefab;
+
+	//Tile Initialization
+	public void InitializeBoard(){
+		Vector3 boardRootPosition = transform.position;
+		for (int i = 0; i > -1*(boardLength+2); i--) {
+			for (int j = 0; j < boardWidth+2; j++) {
+				GameObject newBlock = (GameObject) Instantiate(BlockPrefab,boardRootPosition+new Vector3(j,0,i),new Quaternion());
+				BlockData newBlockData = newBlock.GetComponent<BlockData> ();
+				if (newBlockData != null) {
+					newBlockData.x = j;
+					newBlockData.y = i;
+					if (i == 0 || i == -1*(boardWidth+1)) {
+						newBlockData.GenerateBorderTiles ();
+					} else if (j == 0 || j == boardLength + 1) {
+						newBlockData.GenerateBorderTiles ();
+					}
+					else{
+						newBlockData.GenerateTiles (1); //1 as dummy altitude
+					}
+				}
+				newBlock.transform.SetParent (transform);
+			}
+		}	
+	}
+
+	void Start () {
+		InitializeBoard ();
+	}
+}
